9bf813ac852ed1c08aa68dcfd9ee2f8883cb0dad
Yorran Carvalho <yorrancarvalho18@gmail.com>
2023-05-12 18:32:22 -0300

    my first commit to solar system repository

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container'>
+        <div className='sun'></div>
+        <div className='earth'>
+          <div className='moon'></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
